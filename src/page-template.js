294c8095bf802148cb7edd661c7cb508bbb9f26a
swvmpdad <will.g.camp@gmail.com>
2022-07-24 17:29:44 -0500

    finished site

--- src/page-template.js
+++ src/page-template.js
@@ -12,64 +12,52 @@ const generateAbout = aboutText => {
     `;
 };
 
+
 const generateProjects = projectsArr => {
-    const featuredProjects = projectsArr.filter(project => {
-        if (project.feature) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    const nonFeaturedProjects = projectsArr.filter(project => {
-        if (!project.feature) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
-        return `
-            <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(', ')}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on Github</a>
-            </div>
-        `;
-    });
-
-    const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({ name, description, languages, link }) => {
-        return `
-            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(', ')}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on Github</a>
-            </div>
-        `;
-    });
-
     return `
-        <section class="my-3" id="portfolio">
-            <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-            <div class="flex-row justify-space-between">
-            ${featuredProjectHtmlArr.join('')}
-            ${nonFeaturedProjectHtmlArr.join('')}
+      <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+          .filter(({ feature }) => feature)
+          .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 mb-2 bg-dark text-light p-3">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.map(language => language).join(', ')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
             </div>
-        </section>
+          `;
+          })
+          .join('')}
+  
+        ${projectsArr
+          .filter(({ feature }) => !feature)
+          .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
+          })
+          .join('')}
+        </div>
+      </section>
     `;
-};
+  };
+  
 
 module.exports = templateData => {
-    console.log(templateData);
     // destructure projects and about data from tmplateData based on their property key names
     const { projects, about, ...header } = templateData;
 
